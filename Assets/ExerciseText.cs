1acb59de8237546668a7733bbc8b0f1dffb22700
Janine Semmler <syries@gmx.de>
2023-05-29 09:57:56 +0200

    finish ExerciseText view

--- Assets/ExerciseText.cs
+++ Assets/ExerciseText.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class ExerciseText : MonoBehaviour
+public class ExerciseContent : MonoBehaviour
 {
-    public TMP_Text exerciseText;
+    public TMP_Text content;
     // Start is called before the first frame update
-    void Awake()
+
+    void Start()
     {
         int currentLevel = PlayerPrefs.GetInt("level");
-        exerciseText = GetComponent<TextMeshProUGUI>();
+        content = GetComponent<TextMeshProUGUI>();
         if(currentLevel == 0){
-            exerciseText.text = "Das erste Gemälde diente einst als Zielscheibe in einem Schützenverein. Finde ein Gemälde mit zwei Vögeln.";
+            content.text = "Das erste Gemälde diente einst als Zielscheibe in einem Schützenverein. Finde ein Gemälde mit zwei Vögeln.";
         }
         if(currentLevel == 1){
-            exerciseText.text = "Flötenkonzert Aufgabe";
+            content.text = "Flötenkonzert Aufgabe";
         }
         if(currentLevel == 2){
-            exerciseText.text = "Objektaustausch Aufgabe";
+            content.text = "Objektaustausch Aufgabe";
         }
+        GameObject myGO;
+        GameObject myText;
+        Canvas myCanvas;
+        Text text;
+        RectTransform rectTransform;
+
+        // Canvas
+        myGO = new GameObject();
+        myGO.name = "TestCanvas";
+        myGO.AddComponent<Canvas>();
+
+        myCanvas = myGO.GetComponent<Canvas>();
+        myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        myGO.AddComponent<CanvasScaler>();
+        myGO.AddComponent<GraphicRaycaster>();
+
+        // Text
+        myText = new GameObject();
+        myText.transform.parent = myGO.transform;
+        myText.name = "wibble";
+
+        text = myText.AddComponent<Text>();
+        text.font = (Font)Resources.Load("MyFont");
+        text.text = "wobble";
+        text.fontSize = 100;
+
+        // Text position
+        rectTransform = text.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 0, 0);
+        rectTransform.sizeDelta = new Vector2(400, 200);
     }
 }
