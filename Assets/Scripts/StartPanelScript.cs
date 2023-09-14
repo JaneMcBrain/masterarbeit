1c346abd7b0b3faddbab4e7fc0bc237811a0efe6
Janine Semmler <syries@gmx.de>
2023-09-14 10:24:24 +0200

    fix start buttons click events

--- Assets/Scripts/StartPanelScript.cs
+++ Assets/Scripts/StartPanelScript.cs
@@ -14,7 +14,17 @@ public class StartPanelScript : MonoBehaviour
         var rootElement = start_uiDocument.rootVisualElement;
         var location_Button = rootElement.Q<Button>("LocationButton");
         var tour_Button = rootElement.Q<Button>("TourButton");
-        location_Button.clicked += () => LocationPanel.SetActive(true);
-        tour_Button.clicked += () => TourPanel.SetActive(true);
+        location_Button.clicked += () => onLocationClick();
+        tour_Button.clicked += () => onTourClick();
+    }
+
+    private void onLocationClick(){
+        gameObject.SetActive(false);
+        LocationPanel.SetActive(true);
+    }
+
+    private void onTourClick(){
+        gameObject.SetActive(false);
+        TourPanel.SetActive(true);
     }
 }
