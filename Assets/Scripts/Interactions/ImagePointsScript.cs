2832e82db2308316e329a60e6c51602166692ee5
Janine Semmler <syries@gmx.de>
2023-12-03 20:25:44 +0100

    add switchButton for image pointer change

--- Assets/Scripts/Interactions/ImagePointsScript.cs
+++ Assets/Scripts/Interactions/ImagePointsScript.cs
@@ -16,14 +16,12 @@ public class ImagePointsScript : MonoBehaviour
 
     [SerializeField]
     public Sprite[] images;
-    public GameObject StickerPrefab;
     public GameObject WhiteRect;
-    public GameObject YellowRect;
+    public GameObject HighlightRect;
     public GameObject UI;
     private VisualElement uiDocument;
     private int objectIndex = 0;
     private int currentSticker = 0;
-    private VisualElement currentTrackedImage;
 
     private readonly Dictionary<string, GameObject> _instantiatedSticker = new Dictionary<string, GameObject>();
 
@@ -52,9 +50,12 @@ public class ImagePointsScript : MonoBehaviour
 
         uiDocument = UI.GetComponent<UIDocument>().rootVisualElement;
         uiDocument.Q<VisualElement>("ChangeObjectButtons").RemoveFromClassList("hidden");
+        var switchBtn = uiDocument.Q<Button>("TrackedPositionSelect");
+        switchBtn.RemoveFromClassList("hidden");
         var changeBtn1 = uiDocument.Q<Button>("ChangeObject1");
         var changeBtn2 = uiDocument.Q<Button>("ChangeObject2"); 
         var changeBtn3 = uiDocument.Q<Button>("ChangeObject3");
+        switchBtn.clicked += () => onSwitchPosition();
         changeBtn1.clicked += () => onChangeObject(changeBtn1, 0);
         changeBtn2.clicked += () => onChangeObject(changeBtn2, 1);
         changeBtn3.clicked += () => onChangeObject(changeBtn3, 2);
@@ -79,57 +80,58 @@ public class ImagePointsScript : MonoBehaviour
                 {
                     var key = $"{artworkName}_{i}";
                     if (!_instantiatedSticker.ContainsKey(key)){
-                        addRectPointer(trackedImage, positions[i], sizes[i], key, i);
+                        addRectPointer(trackedImage, key, i);
+                    } else {
+                        setGameObjectParams(_instantiatedSticker[key], trackedImage, positions[i], sizes[i]);
                     }
-                    // } else {
-                    //     _instantiatedSticker[key].transform.position = trackedImage.transform.position + positions[i];
-                    //     _instantiatedSticker[key].transform.localScale = trackedImage.transform.localScale + sizes[i];
-                    // }
                 }
             }
         }
     }
 
-    public void addRectPointer(ARTrackedImage image, Vector3 position, Vector3 size, string key, int index)
+    public void addRectPointer(ARTrackedImage image, string key, int index)
     {
         GameObject sticker;
         Transform newTransform = image.transform;
-        Vector3 stickerSize = new Vector3(image.size.x * size.x, image.size.y * size.y, 1f);
-        //image position + position of array + half size of image (unity centers positions)
-        float posX = newTransform.position.x + position.x + (stickerSize.x * 0.5f);
-        float posY = newTransform.position.y + position.y + (stickerSize.y * 0.5f);
-        Vector3 stickerPosition = new Vector3(posX, posY, newTransform.position.z);
-        Debug.Log($"YOLO Image: {key} is at {newTransform.position} and has size {image.size}.");
-
         if (objectIndex != index){
             sticker = Instantiate(WhiteRect, newTransform);
         } else {
-            sticker = Instantiate(YellowRect, newTransform);
+            sticker = Instantiate(HighlightRect, newTransform);
         }
-        sticker.transform.localScale = stickerSize;
-        sticker.transform.position = stickerPosition;
+        setGameObjectParams(sticker, image, positions[index], sizes[index]);
         _instantiatedSticker[key] = sticker;
         _instantiatedSticker[key].SetActive(true);
-
-        // Jetzt kannst du mit width und height arbeiten
-        Debug.Log($"YOLO Sticker {_instantiatedSticker[key].transform.position} and has size {_instantiatedSticker[key].transform.localScale}");
     }
 
-    void setTrackedImageToUI(string objectName)
+    void setGameObjectParams(GameObject currentObject, ARTrackedImage image, Vector3 position, Vector3 size)
     {
-        var imagePath = "Sprites/Artwork/" + objectName;
-        currentTrackedImage.style.backgroundImage = new StyleBackground(Resources.Load<Sprite>(imagePath));
+        Transform imgTransform = image.transform;
+        //calculate size
+        Vector3 stickerSize = new Vector3(image.size.x * size.x, image.size.y * size.y, 1f);
+        //calculate position
+        //image position + position of array +/- half size of image (unity centers positions)
+        float posX = imgTransform.position.x + position.x + (stickerSize.x * 0.5f);
+        float posY = imgTransform.position.y + position.y - (stickerSize.y * 0.5f);
+        Vector3 stickerPosition = new Vector3(posX, posY, imgTransform.position.z);
+        //save new values in Game Object
+        currentObject.transform.localScale = stickerSize;
+        currentObject.transform.position = stickerPosition;
     }
 
-    void onChangeImageClick(){
-        var objectName = getObjectName();
-        setTrackedImageToUI(objectName);
+    void onSwitchPosition(){
+        float alpha = 1.0f;
+        string stickerIndex = getObjectName();
+        if (_instantiatedSticker[stickerIndex].GetComponent<SpriteRenderer>().sprite.name.Contains("Rect")){
+            alpha = 0.5f;
+        }
+        _instantiatedSticker[stickerIndex].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, alpha);
         if (currentSticker + 1 == _instantiatedSticker.Count)
         {
             currentSticker = 0;
         } else {
             currentSticker += 1;
         }
+        _instantiatedSticker[getObjectName()].GetComponent<SpriteRenderer>().color = new Color(0.18f, 0.64f, 0.94f, 0.5f);
     }
 
     string getObjectName(){
@@ -143,6 +145,7 @@ public class ImagePointsScript : MonoBehaviour
         objectIndex = index;
         activateButton(btn);
         var objectName = getObjectName();
+        Debug.Log($"YOLO onChangeObject objectName: {objectName}");
         _instantiatedSticker[objectName].GetComponent<SpriteRenderer>().sprite = images[objectIndex];
         _instantiatedSticker[objectName].GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
     }
